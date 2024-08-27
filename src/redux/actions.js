1d74066ec387e83363ed1e4a15ca54f4923ae40b
Lyuzika <149685540+LiubovAnufriieva@users.noreply.github.com>
2024-08-27 12:23:00 +0300

    Initial commit

---
+++ src/redux/actions.js
@@ -0,0 +1,32 @@
+export const addTask = (text) => {
+    return {
+      type: 'tasks/addTask',
+      payload: {
+        id: crypto.randomUUID(),
+        completed: false,
+        text,
+      },
+    };
+  };
+  
+  export const deleteTask = (taskId) => {
+    return {
+      type: 'tasks/deleteTask',
+      payload: taskId,
+    };
+  };
+  
+  export const toggleCompleted = (taskId) => {
+    return {
+      type: 'tasks/toggleCompleted',
+      payload: taskId,
+    };
+  };
+  
+  export const setStatusFilter = (value) => {
+    return {
+      type: 'filters/setStatusFilter',
+      payload: value,
+    };
+  };
+  
