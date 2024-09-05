f4690be4061874cdb9ce882d40e95d91c1043a58
Lyuzika <lyuzika@gmail.com>
2024-09-05 18:45:02 +0300

    slices add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import css from "./App.module.css";
 import Loader from "./components/Loader/Loader";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
 const PostsPage = lazy(() => import("./pages/PostsPage"));
 const ContextExamplePage = lazy(() => import("./pages/ContextExamplePage"));
 const PostDetailsPage = lazy(() => import("./pages/PostDetailsPage"));
@@ -17,43 +20,85 @@ const PostReviews = lazy(() => import("./components/PostReviews/PostReviews"));
 function App() {
   return (
     <div>
-      <header>
-        <nav className={css.nav}>
-          <NavLink
-            className={({ isActive }) => clsx(css.link, isActive && css.active)}
-            to="/"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => clsx(css.link, isActive && css.active)}
-            to="/posts"
-          >
-            Posts
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => clsx(css.link, isActive && css.active)}
-            to="/context-example"
-          >
-            Context Example
-          </NavLink>
-        </nav>
-      </header>
-      <main>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/posts" element={<PostsPage />} />
-            <Route path="/context-example" element={<ContextExamplePage />} />
-            <Route path="/posts/:postId" element={<PostDetailsPage />}>
-              <Route path="comments" element={<PostComments />} />
-              <Route path="reviews" element={<PostReviews />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </main>
-      <footer>Footer content</footer>
-    </div>
+    <header>
+      <nav className={css.nav}>
+        <NavLink
+          className={({ isActive }) => clsx(css.link, isActive && css.active)}
+          to="/"
+        >
+          Home
+        </NavLink>
+        {isLoggedIn ? (
+          <>
+            <NavLink
+              className={({ isActive }) =>
+                clsx(css.link, isActive && css.active)
+              }
+              to="/posts"
+            >
+              Posts
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                clsx(css.link, isActive && css.active)
+              }
+              to="/context-example"
+            >
+              Context Example
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                clsx(css.link, isActive && css.active)
+              }
+              to="/contacts"
+            >
+              Contacts
+            </NavLink>
+            <div>
+              <p>Hello, {user.name}!</p>
+              <p>Email: {user.email}</p>
+            </div>
+          </>
+        ) : (
+          <>
+            <NavLink
+              className={({ isActive }) =>
+                clsx(css.link, isActive && css.active)
+              }
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                clsx(css.link, isActive && css.active)
+              }
+              to="/register"
+            >
+              Register
+            </NavLink>
+          </>
+        )}
+      </nav>
+    </header>
+    <main>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/posts" element={<PostsPage />} />
+          <Route path="/context-example" element={<ContextExamplePage />} />
+          <Route path="/posts/:postId" element={<PostDetailsPage />}>
+            <Route path="comments" element={<PostComments />} />
+            <Route path="reviews" element={<PostReviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </main>
+    <footer>Footer content</footer>
+  </div>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { profilesReducer } from "./profiles/profilesReducer";
 import { filterReducer } from "./filter/filterReducer";
+import { postsReducer } from "../posts/posts.slice";
+import { authReducer } from "../redux/auth/slice";
+
+const authConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
 export const store = configureStore({
   reducer: {
     profiles: profilesReducer,
     filter: filterReducer,
+    posts: postsReducer,
+    auth: persistReducer(authConfig, authReducer),
   },
-
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
